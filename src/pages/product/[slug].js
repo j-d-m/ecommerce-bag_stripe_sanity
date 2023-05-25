83c36d0b52e593a8267c0a947946bf9e1fbd6caf
Joshua Moir <j.moir.nz@gmail.com>
2023-05-25 13:25:14 +0200

    worked on the slug.js file - implementing product detaits

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -4,18 +4,102 @@
 //**! Convention
 //**  A Dynamic Segment file naming  convention is [folderName] Dynamic Routes can be accessed from the useRouter hook if need be you can use a catch-all segment by using the spread operator on the file name [...folderName] for example, pages/product/[..slug].js will match /product/a but also will match /products/a/b or alternatively create a Optional Catch all by including the the file name in double square brackets [[..slug]]*/
 
-import React from "react";
+import React, { useState } from "react";
 import { client, urlFor } from "../../../lib/client";
+import {
+  AiFillStar,
+  AiOutlineStar,
+  AiOutlinePlus,
+  AiOutlineMinus,
+} from "react-icons/ai";
+import { Product } from "../../../components";
 
 const ProductDetails = ({ product, products }) => {
   // destructing the props of product
   const { image, name, details, price } = product;
+
+  const [index, setIndex] = useState(0);
   return (
     <div>
-      <div className="product-details-container">
+      <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="" />
+            <img
+              src={urlFor(image && image[index])}
+              alt=""
+              className="product-detail-image"
+            />
+          </div>
+          <div className="small-images-container">
+            {image?.map((item, index) => (
+              //passing a dynamic className to the image
+              <img
+                src={urlFor(item)}
+                alt=""
+                className={
+                  index === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(index)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+            {/**  //*todo <input type="radio" id="star5" name="rating" value="5" />
+            //*todo <label for="star5"></label>
+            //*todo <input type="radio" id="star4" name="rating" value="4" />
+            //*todo <label for="star4"></label>
+            //*todo <input type="radio" id="star3" name="rating" value="3" />
+            //*todo <label for="star3"></label>
+           //*todo <input type="radio" id="star2" name="rating" value="2" />
+           //*todo <label for="star2"></label>
+           //*todo <input type="radio" id="star1" name="rating" value="1" />
+        //*todo <label for="star1"></label> */}
+          </div>
+          <h4>Details</h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />{" "}
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />{" "}
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like these products</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
